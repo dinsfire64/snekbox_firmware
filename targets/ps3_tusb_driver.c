a9b9226f5a3f6c53228e8e08422b74b53730730c
din <dinsfire64@gmail.com>
2026-06-02 07:40:25 -0500

    comment fixes

--- targets/ps3_tusb_driver.c
+++ targets/ps3_tusb_driver.c
@@ -56,88 +56,36 @@ bool ps3d_control_request_cb(uint8_t rhport, uint8_t stage, tusb_control_request
 {
     if (request->wIndex == itf_num && stage == CONTROL_STAGE_SETUP)
     {
-
         switch (request->bRequest)
         {
-            // hid get descriptor
         case 0x06:
+            // hid get descriptor
             tud_control_xfer(rhport, request, &PS3_HID_Report, sizeof(PS3_HID_Report));
             break;
 
         case 0x01:
             if (request->bmRequestType == 0xA1)
             {
-                DebugPrintf("Ps3 Magic!");
+                // required for the console to allow us to press the playstation button
+                DebugPrintf("PS3 Magic!");
                 tud_control_xfer(rhport, request, &ps3_magic_bytes, sizeof(ps3_magic_bytes));
             }
             break;
 
-        // get/set idle, not supported.
         case 0x0A:
         case 0x0B:
+            // get/set idle, not supported.
             return false;
             break;
 
         default:
+            // unknown control transfer request.
             DebugPrintf("ps3d_ctrl req:%02x reqtype:%02x wV:%04x Len:%d",
                         request->bRequest, request->bmRequestType, request->wValue, request->wLength);
             break;
         }
-
-        /*
-        switch (request->bRequest)
-        {
-        case XID_REQ_GET_DESCRIPTOR:
-            if (request->bmRequestType == XID_MSFT_VENDREQ && request->wValue == XID_GET_DESCRIPTOR_VALUE)
-            {
-                tud_control_xfer(rhport, request, &XID_DESCRIPTOR, sizeof(XID_DESCRIPTOR));
-            }
-            break;
-
-        case HID_REQ_CONTROL_GET_REPORT:
-            if (request->bmRequestType == XID_MSFT_VENDREQ)
-            {
-                if (request->wValue == XID_GETCAP_IN)
-                {
-                    tud_control_xfer(rhport, request, &XID_CAP_IN, sizeof(XID_CAP_IN));
-                }
-                else if (request->wValue == XID_GETCAP_OUT)
-                {
-                    tud_control_xfer(rhport, request, &XID_CAP_OUT, sizeof(XID_CAP_OUT));
-                }
-            }
-            else
-            {
-                if (request->bmRequestType == 0xA1 && request->wValue == 0x0100)
-                {
-                    ps3_makeReport();
-
-                    if (request->wLength != sizeof(xbox_controller_state))
-                    {
-                        DebugPrintf("xbox og %d != %d", request->wLength, sizeof(xbox_controller_state));
-                    }
-
-                    tud_control_xfer(rhport, request, &xbox_controller_state, request->wLength);
-                }
-            }
-
-        case HID_REQ_CONTROL_SET_REPORT:
-            if (request->bmRequestType == 0x21 && request->wValue == 0x0200)
-            {
-                tud_control_xfer(rhport, request, xbox_input_data, request->wLength);
-            }
-            break;
-
-            break;
-
-        default:
-            DebugPrintf("UNKNOWN bRequest: %d", request->bRequest);
-            // stall for unimplemented calls is how the original xbox controller worked.
-            return false;
-            break;
-        }
-        */
     }
+
     return true;
 }
 
@@ -147,6 +95,7 @@ static bool ps3d_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, 
 
     if (ep_addr == PS3_IN_EPADDR && result == XFER_RESULT_SUCCESS)
     {
+        // we do not have an endpoint for the rumble right now.
         // ps3_process_output(xferred_bytes);
     }
 

5474cbd82b0d5d1109957880b57e842f7641b43a
din <5580979+dinsfire64@users.noreply.github.com>
2026-08-12 14:57:08 -0500

    joybus refactor for better mode support (#15)

--- targets/joybus.h
+++ targets/joybus.h
@@ -6,61 +6,53 @@
 
 #pragma pack(push, 1)
 
+typedef struct
+{
+    uint8_t a : 1;
+    uint8_t b : 1;
+    uint8_t x : 1;
+    uint8_t y : 1;
+    uint8_t start : 1;
+    uint8_t origin : 1;
+    uint8_t errlatch : 1;
+    uint8_t errstat : 1;
+
+    uint8_t dLeft : 1;
+    uint8_t dRight : 1;
+    uint8_t dDown : 1;
+    uint8_t dUp : 1;
+    uint8_t z : 1;
+    uint8_t r : 1;
+    uint8_t l : 1;
+    uint8_t use_origin : 1;
+} GCButtons;
+
+typedef struct
+{
+    GCButtons btn;
+
+    uint8_t xStick;
+    uint8_t yStick;
+} GCCommon;
+
+// note the bit structures for this can be a bit odd
+// see dolphin for orientation:
+// https://github.com/dolphin-emu/dolphin/blob/144d19433aa734c19c34e5978a1b817d2aa12663/Source/Core/Core/HW/SI/SI_DeviceGCController.cpp#L181
+
 typedef union
 {
+    // pokemon xd gale of darkness and
+    // luigi's mansion use mode 0.
     struct
     {
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t start : 1;
-        uint8_t origin : 1; // Indicates if GetOrigin(0x41) was called (LOW)
-        uint8_t errlatch : 1;
-        uint8_t errstat : 1;
+        GCCommon common;
 
-        uint8_t dLeft : 1;
-        uint8_t dRight : 1;
-        uint8_t dDown : 1;
-        uint8_t dUp : 1;
-        uint8_t z : 1;
-        uint8_t r : 1;
-        uint8_t l : 1;
-        uint8_t use_origin : 1; // some games will ignore analog if this is zero.
-
-        uint8_t xStick;
-        uint8_t yStick;
-        uint8_t cxStick;
-        uint8_t cyStick;
-        uint8_t analogL;
-        uint8_t analogR;
-    }; // mode3 (default reading mode)
-
-    struct
-    {
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t start : 1;
-        uint8_t origin : 1; // Indicates if GetOrigin(0x41) was called (LOW)
-        uint8_t errlatch : 1;
-        uint8_t errstat : 1;
-
-        uint8_t dLeft : 1;
-        uint8_t dRight : 1;
-        uint8_t dDown : 1;
-        uint8_t dUp : 1;
-        uint8_t z : 1;
-        uint8_t r : 1;
-        uint8_t l : 1;
-        uint8_t use_origin : 1;
-
-        uint8_t xStick;
-        uint8_t yStick;
         uint8_t cxStick;
         uint8_t cyStick;
 
+        // note how R and L are reversed
+        // this can be tested using luigi's mansion and the vacuum.
+        // this is true for all nibble based byte words on this architecture.
         uint8_t analogR : 4;
         uint8_t analogL : 4;
 
@@ -68,93 +60,61 @@ typedef union
         uint8_t analogA : 4;
     } mode0;
 
+    // TODO: find game that uses mode 1 reads.
     struct
     {
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t start : 1;
-        uint8_t origin : 1; // Indicates if GetOrigin(0x41) was called (LOW)
-        uint8_t errlatch : 1;
-        uint8_t errstat : 1;
+        GCCommon common;
 
-        uint8_t dLeft : 1;
-        uint8_t dRight : 1;
-        uint8_t dDown : 1;
-        uint8_t dUp : 1;
-        uint8_t z : 1;
-        uint8_t r : 1;
-        uint8_t l : 1;
-        uint8_t use_origin : 1;
-
-        uint8_t xStick;
-        uint8_t yStick;
         uint8_t cyStick : 4;
         uint8_t cxStick : 4;
+
         uint8_t analogL;
         uint8_t analogR;
+
         uint8_t analogB : 4;
         uint8_t analogA : 4;
     } mode1;
 
+    // DDR Mario Mix and Hottest Party reads mode 2
     struct
     {
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t start : 1;
-        uint8_t origin : 1; // Indicates if GetOrigin(0x41) was called (LOW)
-        uint8_t errlatch : 1;
-        uint8_t errstat : 1;
+        GCCommon common;
 
-        uint8_t dLeft : 1;
-        uint8_t dRight : 1;
-        uint8_t dDown : 1;
-        uint8_t dUp : 1;
-        uint8_t z : 1;
-        uint8_t r : 1;
-        uint8_t l : 1;
-        uint8_t use_origin : 1;
-
-        uint8_t xStick;
-        uint8_t yStick;
         uint8_t cyStick : 4;
         uint8_t cxStick : 4;
+
         uint8_t analogR : 4;
         uint8_t analogL : 4;
+
         uint8_t analogA;
         uint8_t analogB;
     } mode2;
 
+    // mode 3 is the default reading mode
     struct
     {
-        uint8_t a : 1;
-        uint8_t b : 1;
-        uint8_t x : 1;
-        uint8_t y : 1;
-        uint8_t start : 1;
-        uint8_t origin : 1; // Indicates if GetOrigin(0x41) was called (LOW)
-        uint8_t errlatch : 1;
-        uint8_t errstat : 1;
+        GCCommon common;
 
-        uint8_t dLeft : 1;
-        uint8_t dRight : 1;
-        uint8_t dDown : 1;
-        uint8_t dUp : 1;
-        uint8_t z : 1;
-        uint8_t r : 1;
-        uint8_t l : 1;
-        uint8_t use_origin : 1;
+        uint8_t cxStick;
+        uint8_t cyStick;
+        uint8_t analogL;
+        uint8_t analogR;
+    } mode3;
 
-        uint8_t xStick;
-        uint8_t yStick;
+    // DDR II on the wii reads mode 4.
+    // if analogA/analogB are not 0xFF, then the game will not let doubles mode play
+    // it will mirror p1's side on p2.
+    struct
+    {
+        GCCommon common;
+
         uint8_t cxStick;
         uint8_t cyStick;
         uint8_t analogA;
         uint8_t analogB;
     } mode4;
+
+    uint8_t raw[8];
 
 } GCReport;
 
